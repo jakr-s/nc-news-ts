b85ccb1a317ce9e810dea3d0720ba1bc7cba53b8
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2024-11-19 16:07:12 +0000

    Add type annotations to API functions

--- src/components/api/index.ts
+++ src/components/api/index.ts
@@ -1,81 +1,99 @@
 import axios from "axios";
+import Article from "../../types/Article";
+import Comment from "../../types/Comment";
+import Topic from "../../types/Topic";
 
 const api = axios.create({
   baseURL: "https://be-nc-news-z0a0.onrender.com/api",
 });
 
-export function fetchArticles() {
+export function fetchArticles(): Promise<Article[]> {
   return api
     .get("/articles")
     .then(({ data }) => {
       if (!data.articles) {
         throw new Error("Articles not found");
       }
-      return data.articles;
+      return data.articles as Article[];
     })
     .catch((error) => {
       throw error.response?.data?.msg || "Articles not found";
     });
 }
 
-export function fetchArticleById(article_id) {
+export function fetchArticleById(article_id: number): Promise<Article> {
   return api
     .get(`/articles/${article_id}`)
     .then(({ data }) => {
       if (!data.article) {
         throw new Error("Article not found");
       }
-      return data.article;
+      return data.article as Article;
     })
     .catch((error) => {
       throw error.response?.data?.msg || "Article not found";
     });
 }
 
-export function fetchCommentsByArticle(article_id) {
+export function fetchCommentsByArticle(article_id: number): Promise<Comment[]> {
   return api
     .get(`/articles/${article_id}/comments`)
-    .then(({ data }) => data.comments)
+    .then(({ data }) => data.comments as Comment[])
     .catch((error) => {
       throw error.response?.data?.msg || "Failed to fetch comments";
     });
 }
 
-export function updateArticleVotes(article_id, inc_votes) {
+export function updateArticleVotes(
+  article_id: number,
+  inc_votes: number,
+): Promise<Article> {
   return api
     .patch(`/articles/${article_id}`, { inc_votes })
-    .then(({ data }) => data.article)
+    .then(({ data }) => data.article as Article)
     .catch((error) => {
-      console.error("Error updating article votes", error);
+      throw error.response?.data?.msg || "Failed to update article votes";
     });
 }
 
-export function updateCommentVotes(comment_id, inc_votes) {
+export function updateCommentVotes(
+  comment_id: number,
+  inc_votes: number,
+): Promise<Comment> {
   return api
     .patch(`/comments/${comment_id}`, { inc_votes })
-    .then(({ data }) => data.comment)
+    .then(({ data }) => data.comment as Comment)
     .catch((error) => {
-      console.error("Error updating comment votes", error);
+      throw error.response?.data?.msg || "Failed to update comment votes";
     });
 }
 
-export function postComment(article_id, username, body) {
+export function postComment(
+  article_id: number,
+  username: string,
+  body: string,
+): Promise<Comment> {
   return api
     .post(`/articles/${article_id}/comments`, { username, body })
-    .then(({ data }) => data.comment)
+    .then(({ data }) => data.comment as Comment)
     .catch((error) => {
       throw error.response?.data?.msg || "Failed to post comment";
     });
 }
 
-export function deleteComment(comment_id) {
-  return api.delete(`/comments/${comment_id}`);
+export function deleteComment(comment_id: number): Promise<void> {
+  return api
+    .delete(`/comments/${comment_id}`)
+    .then(() => {})
+    .catch((error) => {
+      throw error.response?.data?.msg || "Failed to delete comment";
+    });
 }
 
-export function fetchTopics() {
+export function fetchTopics(): Promise<Topic[]> {
   return api
     .get("/topics")
-    .then(({ data }) => data.topics)
+    .then(({ data }) => data.topics as Topic[])
     .catch(() => {
       throw new Error("Failed to fetch topics.");
     });
