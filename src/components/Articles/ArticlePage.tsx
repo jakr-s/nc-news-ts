bec6b0675f9d4ef8451e06f243af1067936bcb6b
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2024-11-19 16:41:15 +0000

    Add type safety to ArticlePage by specifying article_id as a string and converting it to a number

--- src/components/Articles/ArticlePage.tsx
+++ src/components/Articles/ArticlePage.tsx
@@ -5,12 +5,15 @@ import "./Styles/ArticlePage.css";
 
 export default function ArticlePage() {
   const currentUser = "grumpy19"; // Hardcoded user
-  const { article_id } = useParams();
+  const { article_id } = useParams<{ article_id: string }>();
 
   return (
     <div className="article-page">
-      <ArticleContent article_id={article_id} />
-      <CommentSection article_id={article_id} currentUser={currentUser} />
+      <ArticleContent article_id={Number(article_id)} />
+      <CommentSection
+        article_id={Number(article_id)}
+        currentUser={currentUser}
+      />
     </div>
   );
 }
